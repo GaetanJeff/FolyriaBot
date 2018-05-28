5e7c66665e9b05c2d032b6b88fb4f4fa020b0c34
GaetanJeff <30666743+GaetanJeff@users.noreply.github.com>
2018-05-28 05:38:02 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: *help");
+    console.log("Connecté");
+});
+
+bot.login("NDUwNDkwNTg3NDQ4MjEzNTA2.Dez_pA.TQBVy4AiM4JJ2F7AXNdtBZXCIg0");
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.send("Liste des commandes: \n -*help");
+    }
+    
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande de Salut Faite");
+    }
+});
